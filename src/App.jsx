0432180239942a1704e462cf07d2f8c056132bf7
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-05-09 19:42:58 +0600

    react,tailwind css and daisyui setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+  
+  return (
+   
+    <>
+    <h1 className='text-3xl font-bold'>Voluenteer Management Website</h1>
+    </>
+   
+    
+  )
+}
+
+export default App
